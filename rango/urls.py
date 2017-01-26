5dc8ab43fe9cac5fe22edcffdb605753fb24b17e
Millie Norton <2192245N@student.gla.ac.uk>
2017-01-26 15:46:11 +0000

    End of Chapter 4

--- rango/urls.py
+++ rango/urls.py
@@ -1,8 +1,10 @@
 from django.conf.urls import url
 from rango import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIAROOT)
 
 # Create your views here.
--- rango/views.py
+++ rango/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
+    context_dict = {'boldmessage': "Crunchy, creamy, cookie, candy, cupcake!"}
+    return render(request, 'rango/index.html', context=context_dict)
     return HttpResponse("Rango says her there partner!")
 
 # Create your views here.
